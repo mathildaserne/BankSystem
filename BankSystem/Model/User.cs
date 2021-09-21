b9945e7e5dcf70e9fc641c46fc253e9ed46e2f3e
Zia Alekozai <88ziaale@gafe.molndal.se>
2021-09-21 14:24:57 +0200

    Skapat klasser

---
+++ BankSystem/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.Model
+    {
+    class User
+        {
+        public double Balance { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        }
+    }
---
+++ BankSystem/View/InputOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.View
+    {
+    class InputOutput
+        {
+        }
+    }
