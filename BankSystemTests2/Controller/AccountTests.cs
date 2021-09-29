ba2d095623ef89a226aed7081efa9b8f56af8950
ziaalekozai <88ziaale@gafe.molndal.se>
2021-09-29 15:56:48 +0200

    Acctions testning

---
+++ BankSystemTests2/Controller/AccountTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankSystem.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankSystem.Controller.Tests
+    {
+    [TestClass()]
+    public class AccountTests
+        {
+        [TestMethod()]
+        public void removeAccountTest()
+            {
+            Assert.Fail();
+            }
+        }
+    }
