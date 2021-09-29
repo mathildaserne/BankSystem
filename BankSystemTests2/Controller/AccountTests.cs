a30db1a12b539ba0ab954a4ec34b8efaf778234d
ziaalekozai <88ziaale@gafe.molndal.se>
2021-09-29 16:02:25 +0200

    Gjort Unit-test

--- BankSystemTests2/Controller/AccountTests.cs
+++ BankSystemTests2/Controller/AccountTests.cs
@@ -5,17 +5,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankSystem.Model;
 
 
 namespace BankSystem.Controller.Tests
     {
+
     [TestClass()]
     public class AccountTests
         {
         [TestMethod()]
-        public void removeAccountTest()
+        public void IsUserNameString()
             {
-            Assert.Fail();
+            var userList = new User();
+            var actual = userList.UserName = "";
+
+            Assert.IsTrue(true,actual);
+            }
+        //public void IsUserNameNotString()
+        //    {
+        //    var userList = new User();
+        //    var actual = userList.UserName = "2,2";
+
+        //    Assert.IsTrue(true,actual);
+        //    }
+        [TestMethod()]
+        public void IsPasswordString()
+            {
+            var userList = new User();
+            var actual = userList.Password = "";
+
+            Assert.IsTrue(true,actual);
+            }
+
+        //[TestMethod()]
+        //public void IsSalareDouble()
+        //    {
+        //    var userList = new User();
+        //     double actual = userList.Salary = 10.10;
+        //    double expected = double;
+        //    Assert.AreEqual(expected, actual);
+        //    }
+
+        [TestMethod()]
+        public void IsRoleString()
+            {
+            var userList = new User();
+            var actual = userList.Role = "";
+
+            Assert.IsTrue(true,actual);
             }
         }
     }
